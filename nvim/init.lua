f58c044a3aaa26940a6e7d69af3692a2357a25f7
Anton Muravev <tsb99x@gmail.com>
2026-01-07 14:27:26 +0300

    Add nvim-lint plugin

--- nvim/init.lua
+++ nvim/init.lua
@@ -87,6 +87,25 @@ require("lazy").setup({
         lspconfig.clangd.setup({})
       end,
     },
+    {
+      "mfussenegger/nvim-lint",
+      config = function()
+        local lint = require("lint")
+
+        lint.linters_by_ft = {
+          html = { "htmlhint" },
+          dockerfile = { "hadolint" },
+          sh = { "shellcheck" },
+          bash = { "shellcheck" },
+        }
+
+        vim.api.nvim_create_autocmd({ "BufWritePost", "BufReadPost", "InsertLeave" }, {
+          callback = function()
+            lint.try_lint()
+          end,
+        })
+      end,
+    },
   },
   install = { colorscheme = { "habamax" } },
   checker = { enabled = true },
