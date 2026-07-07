e8f24dba526acb6d01df82cc49897eb06a8aded5
Anton Muravev <tsb99x@gmail.com>
2026-07-07 23:09:01 +0300

    Replace music fish function with bash script

--- .config/fish/functions/music.fish
+++
@@ -1,3 +0,0 @@
-function music --wraps="find ~/Music/ -type f -name '*.flac' -or -name '*.m4a' -or -name '*.m4v' -or -name '*.mp3' -or -name '*.opus' -print0 | xargs -0 mpv --shuffle --no-audio-display" --description "alias music=find ~/Music/ -type f -name '*.flac' -or -name '*.m4a' -or -name '*.m4v' -or -name '*.mp3' -or -name '*.opus' -print0 | xargs -0 mpv --shuffle --no-audio-display"
-  find ~/Music/ -type f -name '*.flac' -or -name '*.m4a' -or -name '*.m4v' -or -name '*.mp3' -or -name '*.opus' -print0 | xargs -0 mpv --shuffle --no-audio-display $argv
-end
